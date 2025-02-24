ddbf6f7ed3f6499e38a28ed04814ea6eb5535b55
TheRohit <rohitmeshram000@gmail.com>
2025-02-25 02:25:39 +0530

    redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default async function HomePage() {
-  return (
-    <div className="flex h-full flex-col items-center justify-center p-4">
-      hello
-    </div>
-  );
+  redirect("/dashboard");
 }
